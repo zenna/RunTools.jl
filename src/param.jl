3baa67d3a5d460d02714ad4d729890f5f52fd466
Zenna Tavares <zennatavares@gmail.com>
2018-05-02 23:17:57 -0400

    params

--- src/param.jl
+++ src/param.jl
@@ -40,14 +40,14 @@ end
 dag(v, ω) = v
 dag(v::Params, ω) = v(ω)
 gag(v, ω) = v
-gag(v::RandVar, ω) = dag(v(ω), ω)
+gag(v::Mu.RandVar, ω) = dag(v(ω), ω)
 gag(v::Params, ω) = v(ω)
 
-function (φ::Params)(ω::Omega)
+function (φ::Params)(ω::Mu.Omega)
   Params(Dict(k => gag(v, ω) for (k, v) in φ.d))
 end
 Base.rand(ω, φ::Params) = φ(ω)
-Base.rand(φ::Params) = Base.rand(DefaultOmega(), φ)
+Base.rand(φ::Params) = Base.rand(Mu.DefaultOmega(), φ)
 
 ## Show
 ## ====
